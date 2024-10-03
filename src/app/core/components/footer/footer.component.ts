bb082e16b37d0518a7a08cb0a9aa7d5506f16d2a
Ege-Rita Noor <egenoor@users.noreply.github.com>
2024-10-03 12:22:01 +0300

    Add footer, edit some texts

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core'
 @Component({
   selector: 'footer-layout',
   templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss',
   standalone: true
 })
 export class FooterComponent {}
